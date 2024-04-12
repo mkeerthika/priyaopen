e2d99908328cd65c95e3383761b08e6c62225282
mkeerthika <163974805+mkeerthika@users.noreply.github.com>
2024-04-12 14:19:53 +0530

    Create new.c

---
+++ new.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+  int n, original;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    original = n;
+
+    // reversed integer is stored in reversed variable
+    while (n ! {
+        remaind;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+
+    // palindrome if orignal and reversed are equal
+    if (originareversed)
+        printf("%d is a palindrome.", original);
+    else
+        printf not a palindrome.", original);
+
+    return 0;
+}
